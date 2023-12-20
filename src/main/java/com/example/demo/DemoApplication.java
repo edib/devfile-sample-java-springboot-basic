b0468a9a6e84efda0a5333672ca29127b1668fe9
ibrahim edib kokdemir <kokdemir@gmail.com>
2023-12-20 23:55:29 +0300

    logging

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
+ 
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
     @RequestMapping("/")
     String home() {
+        logger.info("home() method called"); // Log a message when home() is called
         return "Merhaba OpenShift!";
     }
 
